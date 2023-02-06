028164ffb6834aa65e66aea815f81b123c1a166c
신예진 <sak71374@gmail.com>
2023-02-07 02:46:50 +0900

    0207

--- n/n.js
+++ n/n.js
@@ -1,29 +1,26 @@
-$(function() {
+<script>
+$(function(){
+    //resize: 브라우저 창 너비의 변경된 값을 width 변수에 저장
+    $(window).resize(function () {
+        var width = $(window).width();
+        if (width>=992) {
+            $(".box").css("width","50%");
+            $("#news2").css("width","50%");
+            $("#product1").css("width","50%");
+            $("#product2").css("width","50%");
+        } else if (width>=768 && width<992) {
+            $(".box").css("width","100%");
+            $("#news2").css("width","100%");
+            $("#product1").css("width","50%");
+            $("#product2").css("width","50%");
+        } else if (width<768) {
+            $(".box").css("width","100%");
+            $("#news2").css("width","100%");
+            $("#product1").css("width","100%");
+            $("#product2").css("width","100%");
+        }
+    });
 
-    //    $('#side-menu').metisMenu();
-    
-    });
-    
-    //Loads the correct sidebar on window load,
-    //collapses the sidebar on window resize.
-    // Sets the min-height of #page-wrapper to window size
-    $(function() {
-        $(window).bind("load resize", function() {
-            topOffset = 50;
-            width = (this.window.innerWidth > 0) ? this.window.innerWidth : this.screen.width;
-            if (width < 768) {
-                $('div.navbar-collapse').addClass('collapse')
-                topOffset = 100; // 2-row-menu
-            } else {
-                $('div.navbar-collapse').removeClass('collapse')
-            }
-    
-            height = (this.window.innerHeight > 0) ? this.window.innerHeight : this.screen.height;
-            height = height - topOffset;
-            if (height < 1) height = 1;
-            if (height > topOffset) {
-                $("#page-wrapper").css("min-height", (height) + "px");
-            }
-        })
-    });
-    
+    $(window).trigger("resize"); //강제로 호출하는 함수
+});
+</script>
